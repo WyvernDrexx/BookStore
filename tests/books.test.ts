fe47e476a2c668639db9dac78dbcece8c3c31e99
WyvernDrexx <lilaprashadsharma@gmail.com>
2021-01-29 18:21:48 +0530

    add test for books

--- tests/books.test.ts
+++ tests/books.test.ts
@@ -4,7 +4,7 @@ beforeEach(() => {
   graphqlClient.setHeader("Authorization", bearerToken);
 });
 
-describe("books", () => {
+describe("book/s", () => {
   const randomBookName = `_test_book${Math.trunc(Math.random() * 100000)}`;
   test("should not be null when queried", async () => {
     const { books } = await graphqlClient.request(gql`
@@ -37,5 +37,35 @@ describe("books", () => {
     `);
     expect(createBook.name).toBe(randomBookName);
     expect(createBook.price).toBe(490.9);
+  });
+
+  test("cannot be created when not logged in/token is invalid", async () => {
+    graphqlClient.setHeader("Authorization", "");
+    try {
+      const data = await graphqlClient.request(gql`
+      mutation {
+        createBook(
+          data: {
+            name: "${randomBookName}"
+            price: 490.90
+            totalPages: 44
+          }
+        ) {
+          id
+          name
+          price
+          totalPages
+        }
+      }
+    `);
+      expect(data.createBook).toBeUndefined();
+    } catch (error) {
+      expect(error.response.data).toBeNull();
+      expect(error.response.errors).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ code: "UNAUTHENTICATED" }),
+        ])
+      );
+    }
   });
 });
